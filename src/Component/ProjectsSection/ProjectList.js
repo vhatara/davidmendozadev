817b2ef17d100512ae52b93c6dfd6806d8f8e72a
vhatara <davidmndza98@gmail.com>
2023-10-09 00:40:10 +0800

    project: tokenjet

--- src/Component/ProjectsSection/ProjectList.js
+++ src/Component/ProjectsSection/ProjectList.js
@@ -1,42 +1,43 @@
 import jollidupe from "../../Assets/Images/ProjectScreenshots/jollidupe.png";
 import loginsignup from "../../Assets/Images/ProjectScreenshots/login_signup.png"
 import ncmhpharmacy from "../../Assets/Images/ProjectScreenshots/ncmh_pharmacy.png"
+import tokenjet from "../../Assets/Images/ProjectScreenshots/tokenjet-preview.png"
 // import nopreview from "../../Assets/Images/ProjectScreenshots/no-preview.png"
 
 
 const projectListArray = [
+    // {
+    //     id: 1,
+    //     image: jollidupe,
+    //     name: "Jollibee Site Clone",
+    //     description: "A recreation of the Jollibee website to showcase skills on HTMl, CSS, JS, and Bootstrap",
+    //     weblink: "https://vhatara.github.io/jollidupe/",
+    //     sourcecode: "https://github.com/vhatara/jollidupe"
+    // },
+    // {
+    //     id: 2,
+    //     image: loginsignup,
+    //     name: "Login/Signup pages",
+    //     description: "Sample pages for login and signup to practice design",
+    //     weblink: "https://vhatara.github.io/login_signup/",
+    //     sourcecode: "https://github.com/vhatara/login_signup"
+    // },
+    // {
+    //     id: 3,
+    //     image: ncmhpharmacy,
+    //     name: "NCMH Online Pharmacy",
+    //     description: "A capstone project under the KodeGo Full-Stack Web Development Bootcamp",
+    //     weblink: "https://digital-doers.vercel.app/",
+    //     sourcecode: "https://github.com/Keith0309/DigitalDoers"
+    // },
     {
         id: 1,
-        image: jollidupe,
-        name: "Jollibee Site Clone",
-        description: "A recreation of the Jollibee website to showcase skills on HTMl, CSS, JS, and Bootstrap",
-        weblink: "https://vhatara.github.io/jollidupe/",
-        sourcecode: "https://github.com/vhatara/jollidupe"
+        image: tokenjet,
+        name: "TokenJet",
+        description: "TokenJet is cryptocurrency website using CoinGecko API.",
+        weblink: "https://tokenjet.vercel.app/",
+        sourcecode: "https://github.com/vhatara/tokenjet"
     },
-    {
-        id: 2,
-        image: loginsignup,
-        name: "Login/Signup pages",
-        description: "Sample pages for login and signup to practice design",
-        weblink: "https://vhatara.github.io/login_signup/",
-        sourcecode: "https://github.com/vhatara/login_signup"
-    },
-    {
-        id: 3,
-        image: ncmhpharmacy,
-        name: "NCMH Online Pharmacy",
-        description: "A capstone project under the KodeGo Full-Stack Web Development Bootcamp",
-        weblink: "https://digital-doers.vercel.app/",
-        sourcecode: "https://github.com/Keith0309/DigitalDoers"
-    },
-    // {
-    //     id: 4,
-    //     image: nopreview,
-    //     name: "No previews available",
-    //     description: "Lorem ipsum dolor sit amet, consectet ut labore et justo et justo eu arc arc",
-    //     weblink: "#",
-    //     sourcecode: "#"
-    // },
 ]
 
 export default projectListArray;
